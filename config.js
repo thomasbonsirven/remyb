0a69a20bffe0a48c518cfd431ab09f26fede05dd
thomasbonsirven <thomasbonsirven@gmail.com>
2016-04-30 18:31:01 +0200

    Initiate
    Initiate

---
+++ config.js
@@ -0,0 +1,19 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'ios',
+		wp_ws_url : 'https://feuetgloire.com/wp-appkit-api/ios',
+		theme : 'wpak-theme-swapi-master',
+		version : '0.1',
+		app_title : 'Rémy BAYLE',
+		app_platform : 'ios',
+		gmt_offset : 0,
+		debug_mode : 'off',
+		auth_key : 'og*dvOII)&B&*vE8Dkgk2h&NVtlwe*SOaLpB*Q(U*#ENl7AqY*3(!nM3weLf3dAe',
+		options : {"refresh_interval":0},
+		addons : []
+	};
+
+});
